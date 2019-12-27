244fa60d63ec1b727e7a041f33815a7cc03d32e5
wuzanhong <wuzanhong@deepexi.com>
2019-12-27 16:42:55 +0800

    feat: add input-semver component

--- packages/input-semver/src/input-semver.vue
+++ packages/input-semver/src/input-semver.vue
@@ -9,12 +9,12 @@
  * 如1.11.0
  */
 
-const VERSION_PATTERN = /^v?([1-9]\d*|0)\.([1-9]\d*|0)\.([1-9]\d*|0)$/
+const VERSION_PATTERN = /^v?([1-9]\d*|0)\.([1-9]\d*|0)\.([1-9]\d*|0)$/;
 
 // 默认版本
-const NONSENSE_VERSION = '0.0.0'
-const MIN_VERSION = '0.0.1'
-const DEFAULT_VERSION = '1.0.0'
+const NONSENSE_VERSION = '0.0.0';
+const MIN_VERSION = '0.0.1';
+const DEFAULT_VERSION = '1.0.0';
 const v = 'v'
 
 // 自动填充规则
@@ -43,7 +43,7 @@ const autoPaddingRegularList = [
     regular: /^v?$/,
     padding: DEFAULT_VERSION
   }
-]
+];
 
 const autoFixRegular = [
   {
@@ -78,7 +78,7 @@ const autoFixRegular = [
         .join('.')
     }
   }
-]
+];
 
 export default {
   name: 'ElInputSemver',
@@ -129,68 +129,68 @@ export default {
     return {
       version: this.value,
       isValid: true
-    }
+    };
   },
   watch: {
     // 同步外面传进来的value
     value(data) {
-      this.version = data
+      this.version = data;
     }
   },
   methods: {
     handelInput(e = '') {
-      let hasV = false
+      let hasV = false;
 
       // 如果需要阻止非法输入,则替换除了开头的第一个v，数字，点为空
       if (this.preventIllegal) {
         e = e.replace(/[^\d\.]/g, function(data) {
           if (data == v && e.startsWith(v) && !hasV) {
-            hasV = true
-            return v
+            hasV = true;
+            return v;
           }
-          return ''
+          return '';
         })
       }
 
       // 如果需要前缀且当前value不是v开头,则自动带上v
-      let value = this.prefix && e.length > 0 && !e.startsWith(v) ? v + e : e
-      let isValid = this.checkVersionVaild(value)
+      let value = this.prefix && e.length > 0 && !e.startsWith(v) ? v + e : e;
+      let isValid = this.checkVersionVaild(value);
 
       // this is hack! magic function
       this.$nextTick(() => {
-        this.notify({value, isValid})
+        this.notify({value, isValid});
       })
     },
     // 失去焦点时进行判断
     handleBlur() {
-      let originValue = this.version || ''
+      let originValue = this.version || '';
 
       /**
        * 如果是不完全输入，会自动填充
        */
       // 依次寻找匹配自动填充的规则
       let matchPaddingRegular = this.autoPaddingRegularList.find(item => {
-        return item.regular.test(originValue)
-      })
+        return item.regular.test(originValue);
+      });
 
       // 如果需要自动填充,且存在可以满足的自动填充，则自动填充后续字符串
       if (this.autoPadding && matchPaddingRegular) {
-        let paddedValue = originValue + matchPaddingRegular.padding
+        let paddedValue = originValue + matchPaddingRegular.padding;
 
         // 自动补齐后可能会出现版本0.0.0, 将0.0.0替换为默认版本
-        paddedValue = paddedValue.replace(NONSENSE_VERSION, MIN_VERSION)
+        paddedValue = paddedValue.replace(NONSENSE_VERSION, MIN_VERSION);
 
         // 如果需要前缀，且没有输入前缀v，自动添加前缀
         paddedValue =
           this.prefix && !paddedValue.startsWith(v)
             ? v + paddedValue
-            : paddedValue
+            : paddedValue;
 
         this.notify({
           value: paddedValue
-        })
+        });
 
-        return
+        return;
       }
 
       /**
@@ -198,56 +198,56 @@ export default {
        */
       // 尝试寻找可以自动fix的规则
       let matchFixRegular = autoFixRegular.find(item => {
-        return item.regular.test(originValue)
-      })
+        return item.regular.test(originValue);
+      });
       // 满足条件则autoFix
       if (matchFixRegular) {
-        let fixValue = matchFixRegular.fixStr(originValue)
+        let fixValue = matchFixRegular.fixStr(originValue);
 
         this.notify({
           value: fixValue
-        })
-        return
+        });
+        return;
       }
 
       // 格式不对重置输入框
       if (!this.checkVersionVaild(originValue)) {
-        this.setDefaultVersion()
+        this.setDefaultVersion();
       }
     },
     updateValue(value) {
       /**
        * @property {string} value - 输入框最新的值
        */
-      this.$emit('input', value)
+      this.$emit('input', value);
     },
     updateValid(isValid) {
       /**
        * 最新值是否符合校验规则
        * @property {boolean} isValid
        */
-      this.$emit('validChange', isValid)
-      this.isValid = isValid
+      this.$emit('validChange', isValid);
+      this.isValid = isValid;
     },
     // 对外通知
     notify({value, isValid = true}) {
-      this.updateValue(value)
-      this.updateValid(isValid)
+      this.updateValue(value);
+      this.updateValid(isValid);
     },
     checkVersionVaild(version = '') {
       // 不允许 0.0.0
       return (
         version.split('.').some(item => item != 0) &&
         this.validRegular.test(version)
-      )
+      );
     },
     // 重置value为默认版本（如失去焦点时若格式不对）
     setDefaultVersion() {
       this.notify({
         value: this.prefix ? v + DEFAULT_VERSION : DEFAULT_VERSION
-      })
+      });
     }
   }
-}
+};
 </script>
 
---
+++ types/input-semver.d.ts
@@ -0,0 +1,5 @@
+import { ElementUIComponent } from './component'
+
+/** InputSemver Component */
+export declare class ElInputSemver extends ElementUIComponent {
+}
