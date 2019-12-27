398149a2a3db2b8d0285b8d23ed1c2196691f4f8
wuzanhong <wuzanhong@deepexi.com>
2019-12-27 14:59:53 +0800

    feat: add input-email component

---
+++ packages/input-email/index.js
@@ -0,0 +1,8 @@
+import InputEmail from './src/input-email';
+
+/* istanbul ignore next */
+InputEmail.install = function(Vue) {
+  Vue.component(InputEmail.name, InputEmail);
+};
+
+export default InputEmail;
