3a4000ad3b9cf84ba79c086d67a4e2dc1d0fa93a
wuzanhong <wuzanhong@deepexi.com>
2019-12-30 17:42:55 +0800

    fix: 提供export

--- src/index.js
+++ src/index.js
@@ -95,6 +95,7 @@ import Panel from '../packages/panel/index.js';
 import Skeleton from '../packages/skeleton/index.js';
 import Empty from '../packages/empty/index.js';
 import Affix from '../packages/affix/index.js';
+import InputIdCard from '../packages/input-id-card/index.js';
 import locale from 'element-ui/src/locale';
 import CollapseTransition from 'element-ui/src/transitions/collapse-transition';
 
@@ -188,6 +189,7 @@ const components = [
   Skeleton,
   Empty,
   Affix,
+  InputIdCard,
   CollapseTransition
 ];
 
@@ -324,5 +326,6 @@ export default {
   Panel,
   Skeleton,
   Empty,
-  Affix
+  Affix,
+  InputIdCard
 };
