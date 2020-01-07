db90b3b58eb649792da7b956284febcbac5e79bf
Donald Shen <825870831@qq.com>
2020-01-07 11:56:00 +0800

    feat: 新增 input-url 输入框组件

---
+++ packages/input-url/src/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-input
+    class="el-input-url"
+    :value="value"
+    :maxlength="2000"
+    v-bind="$attrs"
+    :placeholder="$attrs.placeholder || '请输入url'"
+    v-on="$listeners"
+  />
+</template>
+
+<script>
+import validUrl from 'valid-url';
+
+export default {
+  name: 'ElInputUrl',
+  rules: [
+    {
+      validator(r, v, cb) {
+        if (validUrl.isWebUri(v)) cb();
+        else cb(new Error('请输入正确的 URL 地址'));
+      },
+      trigger: 'blur'
+    },
+  ],
+  props: {
+    /**
+     * @model
+     */
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
